480d497fb78067a240baf85ee266aa47525dbae4
Timur Khakkiev <khakkievtimur@yandex.ru>
2021-09-30 13:13:46 +0300

    Оптимизировал изображения

--- src/scripts/galerie.js
+++
@@ -1,6 +0,0 @@
-const contentItems = document.querySelectorAll('.content-item');
-const modalBg = document.querySelectorAll('.modal-bg');
-
-contentItems.forEach(item => {
-	item.addEventListener('click', () => {});
-});
